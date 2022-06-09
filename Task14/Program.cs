ccac03d59f6899bf50da83460c29568ad1b8326b
Mad <ned_91@mail.ru>
2022-06-09 21:23:06 +0300

    add Task 14

---
+++ Task14/Program.cs
@@ -0,0 +1,18 @@
+﻿//**Задача 14:**
+//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+//14  ->  нет
+//46  ->  нет
+//161 ->  да
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("type the first number ");
+int number1 = Convert.ToInt32(ReadLine());
+
+string answer = number1%7 == 0 && number1%23 == 0?"Yes":"No";
+Write(answer);
+
